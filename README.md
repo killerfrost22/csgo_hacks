1c68135c2956a3a0ea36ae9ddbeed34dd92982ad
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-11-05 13:58:55 -0400

    hacks

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # radar_detection_OGSC
  title completes me
+ A CSGO radar hack client
+## How to start
+
+### What do I need 
+--Python
+--Pymem
+
+### What's next
+Start CS:GO by double-clicking activation.py or entering the python activation.py command. Despite the fact that the application is immediately available, the hack only works as long as the game is active. Simply restart activation.py to restart hack without restarting.
---
+++ src/radar/activation.py
@@ -0,0 +1,13 @@
+import pymem
+import re
+
+pm = pymem.Pymem('csgo.exe')
+client = pymem.process.module_from_name(pm.process_handle,
+                                        'client.dll')
+
+clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
+address = client.lpBaseOfDll + re.search(rb'\x83\xE0\x0F\x80\xBF',
+                                         clientModule).start() + 9
+
+pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
+pm.close_process()
